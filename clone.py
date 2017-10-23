4a905adc2779e849bd5217afdb976d3e7faf4a79
Heming Chen <heming.chen@hitachi-automotive.us>
2017-10-23 09:54:19 -0400

    final submission

---
+++ clone.py
@@ -0,0 +1,194 @@
+import csv
+import datetime
+import os
+from shutil import copyfile
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+STEERING_CORRECTION = 0.2
+N_EPOCHS = 3
+IMAGE_SCALING_FACTOR = 0.5
+ADD_FLIPPED_IMAGES = False
+DEFAULT_IMAGE_WIDTH = 320
+DEFAULT_IMAGE_HEIGHT = 160
+DEFAULT_IMAGE_CHANNEL = 3
+SCALED_IMAGE_WIDTH = int(IMAGE_SCALING_FACTOR * DEFAULT_IMAGE_WIDTH)
+SCALED_IMAGE_HEIGHT = int(IMAGE_SCALING_FACTOR * DEFAULT_IMAGE_HEIGHT)
+DRIVING_LOG_CSV_FILE_NAME = 'driving_log.csv'
+ROOT_DATA_FOLDER = './data'
+ROOT_TESTED_MODELS_FOLDER = './tested_models'
+SELECTED_DATA_SETS = [
+    # 'curve_near_beginning_1',
+    # 'curve_near_beginning_2',
+    # 'recovery_driving',
+    'reversed_center_lane_driving_1',
+    # 'curve_after_bridge_1',
+    # 'curve_after_bridge_2',
+    'center_lane_driving_1',
+    'center_lane_driving_2',
+    'center_lane_driving_3',
+    'center_lane_driving_4',
+    'center_lane_driving_5',
+    'center_lane_driving_6',
+    'udacity']
+MODEL_META_FILE_NAME = "meta.txt"
+WINDOWS_PATH_PREFIX_TO_REMOVE = "C:\\Users\\hchen\\Desktop\\windows_sim\\data\\"
+MACOS_PATH_PREFIX_TO_REMOVE = "/Users/frishen/Desktop/udacity/data/"
+
+
+def read_csv_lines(file_path, img_path_prefix=None):
+    lines = []
+    with open(file_path) as csvfile:
+        reader = csv.reader(csvfile)
+        csv_header = next(reader)
+        for line in reader:
+            if img_path_prefix is None:
+                lines.append(line)
+            else:
+                for i in range(0, 3):
+                    # Clean up the image path since data was recorded on Windows/macOS
+                    line[i] = line[i] \
+                        .strip() \
+                        .replace(WINDOWS_PATH_PREFIX_TO_REMOVE, "") \
+                        .replace(MACOS_PATH_PREFIX_TO_REMOVE, "") \
+                        .replace("\\", "/")
+
+                    line[i] = img_path_prefix + line[i]
+                lines.append(line)
+    return lines
+
+
+# sample = line here
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:  # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset + batch_size]
+
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+                # Read in images from center, left and right cameras
+                center_image = cv2.imread(ROOT_DATA_FOLDER + '/' + batch_sample[0])
+                left_image = cv2.imread(ROOT_DATA_FOLDER + '/' + batch_sample[1])
+                right_image = cv2.imread(ROOT_DATA_FOLDER + '/' + batch_sample[2])
+
+                # Convert to RGB since the drive.py sends in RGB images when testing
+                center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
+                left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
+                right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB)
+
+                # Read steering and populate for left and right camera images
+                center_image_steering_measurement = float(batch_sample[3])
+                left_image_steering_measurement = center_image_steering_measurement + STEERING_CORRECTION
+                right_image_steering_measurement = center_image_steering_measurement - STEERING_CORRECTION
+
+                images.extend([
+                    center_image,
+                    left_image,
+                    right_image])
+                measurements.extend([
+                    center_image_steering_measurement,
+                    left_image_steering_measurement,
+                    right_image_steering_measurement])
+
+                if ADD_FLIPPED_IMAGES:
+                    # add flipped images and measurements
+                    center_image_flipped = np.fliplr(center_image)
+                    left_image_flipped = np.fliplr(left_image)
+                    right_image_flipped = np.fliplr(right_image)
+
+                    center_image_steering_measurement_flipped = -center_image_steering_measurement
+                    left_image_flipped_steering_measurement = -left_image_steering_measurement
+                    right_image_flipped_steering_measurement_flipped = -right_image_steering_measurement
+
+                    images.extend([
+                        center_image_flipped,
+                        left_image_flipped,
+                        right_image_flipped])
+                    measurements.extend([
+                        center_image_steering_measurement_flipped,
+                        left_image_flipped_steering_measurement,
+                        right_image_flipped_steering_measurement_flipped])
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+            yield shuffle(X_train, y_train)
+
+
+lines = []
+for selected_data_set in SELECTED_DATA_SETS:
+    lines.extend(read_csv_lines(
+        file_path=ROOT_DATA_FOLDER + '/' + selected_data_set + '/' + DRIVING_LOG_CSV_FILE_NAME,
+        img_path_prefix=selected_data_set + '/'))
+
+print('\ntotal training set size: {}\n'.format(len(lines)))
+
+# Split all lines into train and validation set
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+
+# Get the generators for each set
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+# Use Nvidia's CNN for autonomous driving
+model = Sequential()
+model.add(
+    Lambda(lambda x: x / 255.0 - 0.5, input_shape=(DEFAULT_IMAGE_HEIGHT, DEFAULT_IMAGE_WIDTH, DEFAULT_IMAGE_CHANNEL)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(1))
+
+# Train
+model.compile(loss='mse', optimizer='adam')
+history_object = model.fit_generator(
+    train_generator,
+    samples_per_epoch=len(train_samples),
+    validation_data=validation_generator,
+    nb_val_samples=len(validation_samples),
+    nb_epoch=N_EPOCHS)
+
+# Save model
+model.save('model.h5')
+
+# Archive model
+timestamp = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+model_folder = './' + ROOT_TESTED_MODELS_FOLDER + '/' + timestamp
+if not os.path.exists(model_folder):
+    os.makedirs(model_folder)
+    copyfile('model.h5', model_folder + '/' + 'model.h5')
+    with open(model_folder + '/' + MODEL_META_FILE_NAME, 'a') as f:
+        f.write('selected_data_set:\n')
+        for data_set in SELECTED_DATA_SETS:
+            f.write('  ' + data_set + '\n')
+        f.write('n_epochs: ' + str(N_EPOCHS) + '\n')
+        f.write('add_flipped_images: ' + str(ADD_FLIPPED_IMAGES) + '\n')
+
+# Plot result
+# Print the keys contained in the history object
+print(history_object.history.keys())
+
+# Plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig(model_folder + '/' + 'train_valid_loss.png')
+plt.show()
---
+++ writeup_report.md
@@ -0,0 +1,104 @@
+#**Behavioral Cloning** 
+
+
+###Files Submitted & Code Quality
+
+####1. Submission includes all required files and can be used to run the simulator in autonomous mode
+
+My project includes the following files:
+* model.py containing the script to create and train the model
+* drive.py for driving the car in autonomous mode
+* model.h5 containing a trained convolution neural network
+* writeup_report.md or writeup_report.pdf summarizing the results
+
+####2. Submission includes functional code
+Using the Udacity provided simulator and my drive.py file, the car can be driven autonomously around the track by executing
+```sh
+python drive.py model.h5
+```
+
+####3. Submission code is usable and readable
+
+The model.py file contains the code for training and saving the convolution neural network. The file shows the pipeline I used for training and validating the model, and it contains comments to explain how the code works.
+
+###Model Architecture and Training Strategy
+
+####1. An appropriate model architecture has been employed
+
+I used the NVIDIA convolution neural network, which contains:
+1. 5x5 filter with depth of 24 and RELU activation
+2. 5x5 filter with depth of 36 and RELU activation
+3. 5x5 filter with depth of 48 and RELU activation
+4. 3x3 filter with depth of 64 and RELU activation
+5. 3x3 filter with depth of 64 and RELU activation
+6. Fully connected layer with 100 outputs
+7. Fully connected layer with 50 outputs
+8. Final layer with 1 output
+
+The input data is also normalized and cropped to speed up the training process.
+
+####2. Attempts to reduce overfitting in the model
+
+My model turned out OK without dropout layers.
+
+Instead, I trained the model with both udacity data and my own driving data sets, including one lap driving in the reverse direction, which helped reduce the overfitting (model.py lines 27-40).
+
+
+####3. Model parameter tuning
+
+The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 158).
+
+####4. Appropriate training data
+
+Training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road and also one lap driving in the reverse direction.
+
+For details about how I created the training data, see the next section.
+
+###Model Architecture and Training Strategy
+
+####1. Solution Design Approach
+
+The overall strategy for deriving a model architecture was to reuse the NVIDIA model as much as possible and supply various good quality training data.
+
+My first step was to build the NVIDIA convolution neural network model. After the initial test with just one set of driving data and 3 epochs, the error was down to 0.019, which implied that this model should be a good fit for this project.
+
+To further test the model, I collected more data and split it into a training and validation set. I found that when training for 5 epochs, my model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting.
+
+To combat the overfitting, I reduced the number of epochs to 3, and collected more data, such as "very smooth driving around the curves," "recovery driving," "driving in the reverse direction," etc. I also used udacity data together with my own driving data.
+
+Then I trained the model, it turned out that there was too much oscillation in lateral direction. I thought it might be related to the recovery driving data where I introduced too much sharp turn maneuvers which affected the model.
+
+Therefore in the final step, I removed the recovery driving data set and trained the model with 3 epochs. The model turned out pretty good. It was able to drive the car for multiple laps without falling off the track or even touch the lane markers.
+
+
+####2. Final Model Architecture
+
+The final model architecture (model.py lines 142-155) is the same as the NVIDIA model that consisted of a convolution neural network with the following layers and layer sizes:
+1. 5x5 filter with depth of 24 and RELU activation
+2. 5x5 filter with depth of 36 and RELU activation
+3. 5x5 filter with depth of 48 and RELU activation
+4. 3x3 filter with depth of 64 and RELU activation
+5. 3x3 filter with depth of 64 and RELU activation
+6. Fully connected layer with 100 outputs
+7. Fully connected layer with 50 outputs
+8. Final layer with 1 output
+
+####3. Creation of the Training Set & Training Process
+
+To capture good driving behavior, I first recorded two laps on track one using center lane driving.
+
+I then recorded another two laps driving in the center of the lane.
+
+Later I drove 1 lap using recovery driving.
+
+I later added another 4 sets of center lane driving data.
+
+To augment the data sat, I drove another two laps in the reverse direction.
+
+I didn't flip the image as it would double the training set and I wasn't able to train the model on GPU.
+
+I finally randomly shuffled the data set and put 20% of the data into a validation set.
+
+I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was 3 as the validation error will go up compared to training error if 5 epochs was used.
+
+I eventually used an adam optimizer so that manually training the learning rate wasn't necessary.
